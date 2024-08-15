2ca45e827bc13fe509cb6411fb7f10cf39746bb2
pankaj sondhiya <pankajsondhiya780@gamil.com>
2024-08-15 11:20:36 +0530

    final commit 2

---
+++ src/AxiosInstance/AxiosInstance.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { BASE_URL, TIME_OUT } from "../Constans";
+
+axios.defaults.headers.common["Content-Type"] = "application/json";
+axios.defaults.headers.common["Accept"] = "application/json";
+
+export const AxiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: TIME_OUT,
+});
+
+AxiosInstance.interceptors.request.use(function (config) {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers["x-access-token"] = token;
+  }
+
+  return config;
+});
